a5a87db1af39341fe4d2f56be386920e42e27e6a
minjae1025 <kjt081025@gmail.com>
2025-12-15 00:49:25 +0900

    라우터 재수정...

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import './App.css'
 import { BrowserView, MobileView } from 'react-device-detect'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import HomeRouter from './routes/HomeRouter'
-import LoginRouter from './routes/LoginRouter'
-import { UserProvider } from './services/UserContext';
-
+import HomeRouter from './routers/HomeRouter'
+import LoginRouter from './routers/LoginRouter'
 
 function App() {
   return (
@@ -13,12 +11,10 @@ function App() {
         <h1>PC에서는 제공되지 않습니다.</h1>
       </BrowserView>
       <MobileView>
-        <UserProvider>
-          <Routes>
-            <Route path='/*' element={<HomeRouter />} />
-            <Route path='/login/*' element={<LoginRouter />} />
-          </Routes>
-        </UserProvider>
+        <Routes>
+          <Route path='/*' element={<HomeRouter />} />
+          <Route path='/login/*' element={<LoginRouter />} />
+        </Routes>
       </MobileView>
     </>
   )
